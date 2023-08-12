2d513610a8fb89bbc55a9b9ea9c66c29f41d1427
Fady Philips <fadyaphilips97@gmail.com>
2023-08-12 15:25:25 -0230

    creating the calculate functions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let stacking = false
+let working = ''
+let operator = ''
+
 function add(x, y) {
     return parseFloat(x) + parseFloat(y);
 }
@@ -7,14 +11,19 @@ function subtract(x, y) {
 }
 
 function divide(x, y) {
-    return parseFloat(x) / parseFloat(y);
+    if (y === 0) {
+        stacking = false
+        return ("You are trying to divide by zero! Are you trying to create a black hole?")
+    } else {
+        return parseFloat(x) / parseFloat(y);
+    }
 }
 
 function multiply(x, y) {
     return parseFloat(x) * parseFloat(y);
 }
 
-function operate(operator, a, b) {
+function calculate(operator, a, b) {
     switch (operator) {
         case "+":
             return add(a, b);
@@ -30,9 +39,9 @@ function operate(operator, a, b) {
 }
 
 function enterNumber(number) {
-    let current = document.getElementById("display").value
+    let current = document.getElementById("working").innerHTML
     let newNumber = current + number
-    document.getElementById("display").value = newNumber
+    document.getElementById("working").innerHTML = newNumber
 }
 
 function cancel() {
@@ -43,4 +52,28 @@ function invertSign() {
     let current = parseFloat(document.getElementById("display").value)
     let newNumber = current * -1
     document.getElementById("display").value = newNumber
+}
+
+function operate(op) {
+    let currentDisplay = document.getElementById("display").value
+    let currentWorking = document.getElementById("working").innerHTML
+    let currentOperator = document.getElementById("operator").getAttribute("data-operator")
+
+    if (currentWorking && currentOperator && currentDisplay && stacking) {
+        const answer = calculate(currentOperator, currentWorking, currentDisplay)
+        document.getElementById("working").innerHTML = answer;
+    } else if (currentDisplay) {
+        document.getElementById("working").innerHTML = currentDisplay;
+    }
+
+    if (op != '=') {
+        stacking = true
+        document.getElementById("operator").setAttribute("data-operator", op)
+        document.getElementById("operator").innerHTML = op;
+    } else {
+        stacking = false
+        document.getElementById("operator").innerHTML = '';
+    }
+
+    document.getElementById("display").value = ""
 }
